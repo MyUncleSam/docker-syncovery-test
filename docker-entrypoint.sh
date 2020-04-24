d2b871eea0dcdbd920a377a056f8bcb542939f3b
MyUncleSam <12820919+MyUncleSam@users.noreply.github.com>
2020-04-24 23:28:58 +0200

    add sleep to fix startup troubles (hopefully)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,7 @@
 if [ ! -f /config/.Syncovery/Syncovery.cfg ]; then
     echo "Setting configuration setting for webserver"
     exec /syncovery/SyncoveryCL SET /WEBSERVER=0.0.0.0
+    sleep 10
 fi
 
 echo "Starting Server"
